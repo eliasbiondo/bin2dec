2da999f3bf13b06fe9f97d4d8f17bfb40371a5fe
Elias Biondo <contato@eliasbiondo.com>
2021-08-12 11:21:57 -0300

    :sparkles: Added calc binary to decimal feature

---
+++ public/js/main.js
@@ -0,0 +1,93 @@
+let input = document.querySelector("#input-fieldset input");
+let output = document.querySelector("#output-fieldset .input-block input");
+
+let calc = {
+    mode: "binary_to_decimal",
+    last_input: null,
+    last_output: null,
+}
+
+function isValidNumber(e) {
+
+    switch(calc.mode) {
+
+        case "binary_to_decimal":
+            
+            if (e.keyCode == 48 || e.keyCode == 49) {
+                return true;
+            }
+
+            return false;
+
+        case "decimal_to_binary":
+
+            if (e.keyCode >=48 && e.keyCode <= 57) {
+                return true;
+            }
+            
+            return false;
+        
+    }
+
+}
+
+function calculate() {
+
+    // Refresing input data for auto calculate
+    input = document.querySelector("#input-fieldset input");
+
+    // Checking if exists an input value and if it's diff from last input
+    if(input.value && input.value != calc.last_input) {
+
+        // Assigning last input value how current input vale
+        calc.last_input = input.value;
+        
+        // Calculating
+        switch(calc.mode) {
+
+            case "binary_to_decimal":
+
+                // Instancing the binary number how an array of integers (Eg. 1011 -> [1,0,1,1]). 
+                let binary = Array.from(String(input.value), Number);
+
+                // Reverting the array (Eg. [1,0,1,1] -> [1,1,0,1]) to facilitate the calc
+                binary.reverse();
+
+                // Instancing the decimal value
+                let decimal = 0;
+
+                // Going through the array, summing the decimal corresponding value
+                binary.forEach( (number, index) => {
+                    
+                    // If current number is equals to one, sum their decimal corresponding to 'decimal' variable value
+                    if (number == 1) {
+                        decimal += Math.pow(2, index);
+                    }
+
+                })
+
+                console.log(decimal);
+
+                // Assinging the last output value how the calculated decimal
+                calc.last_output = decimal;
+
+                output.value = decimal;
+
+            break;
+                
+    
+            case "decimal_to_binary":
+    
+            break;
+    
+        }
+
+    } else if (!input.value && calc.last_input) {
+        output.value = null;
+    }
+
+    return;
+}
+
+// Calling "calculte()" function in intervals of 250ms for auto calculate
+setInterval(calculate, 250);
