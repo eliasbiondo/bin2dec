ec7674a9cdfbd6374bb7619c0f0e808e31266a97
Elias Biondo <contato@eliasbiondo.com>
2021-08-12 15:03:21 -0300

    :sparkles: Added calc decimal to binary and swap feature

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,23 @@
 let input = document.querySelector("#input-fieldset input");
-let output = document.querySelector("#output-fieldset .input-block input");
-let clipboard  = document.querySelector(".clipboard");
-let message_box = document.querySelector(".notifications");
+let input_label = document.querySelector("#input-fieldset label");
 
+let output = document.querySelector("#output-fieldset .input-block input");
+let output_label = document.querySelector("#output-fieldset label");
+
+let clipboard  = document.querySelector(".clipboard");
+let notifications = document.querySelector(".notifications");
+
+let swap_btn = document.querySelector(".swap");
+
+// Instancing the decimal and binary calc auxiliary variables 
+let decimal;
+let binary;
+
+// Reseting values on load
+input.value = null;
+output.value = null;
+
+// Instancing the calc object
 let calc = {
     mode: "binary_to_decimal",
     last_input: null,
@@ -50,14 +65,14 @@ function calculate() {
 
             case "binary_to_decimal":
 
-                // Instancing the binary number how an array of integers (Eg. 1011 -> [1,0,1,1]). 
-                let binary = Array.from(String(input.value), Number);
+                // Setting the binary number how an array of integers (Eg. 1011 -> [1,0,1,1]). 
+                binary = Array.from(String(input.value), Number);
 
                 // Reverting the array (Eg. [1,0,1,1] -> [1,1,0,1]) to facilitate the calc
                 binary.reverse();
 
                 // Instancing the decimal value
-                let decimal = 0;
+                decimal = 0;
 
                 // Going through the array, summing the decimal corresponding value
                 binary.forEach( (number, index) => {
@@ -72,14 +87,44 @@ function calculate() {
                 // Assinging the last output value how the calculated decimal
                 calc.last_output = decimal;
 
-                // Printing the result on screen
+                // Printing the result
                 output.value = decimal;
 
             break;
                 
     
             case "decimal_to_binary":
-    
+                
+                // Setting the decimal number how the input value
+                decimal = input.value;
+
+                // Instancing the binary array
+                let binary_arr = [];
+
+                // Instancing the decrease index
+                let decrease_index;
+                
+                // Getting the binary inverted array
+                do {
+
+                    decrease_index = Math.floor(decimal/2);
+
+                    if (decimal % 2 == 0) {
+                        binary_arr.push(0);
+                        decimal = decimal - decrease_index;
+                    } else {
+                        binary_arr.push(1);
+                        decimal = decimal - decrease_index - 1;
+                    }
+
+                } while (decimal > 0);
+
+                // Reversing the binary array to get the real binary number
+                binary_arr.reverse();
+
+                // Printing the result
+                output.value = binary_arr.join('');
+
             break;
     
         }
@@ -97,6 +142,58 @@ setInterval(calculate, 250);
 // Implementing the sleep function
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+swap_btn.addEventListener("click", swap);
+
+// Implementing the swap feature
+function swap() {
+
+    switch(calc.mode) {
+        case "binary_to_decimal":
+
+            calc.mode = "decimal_to_binary";
+            
+            input_label.setAttribute("for","decimal");
+            input_label.innerHTML = "Decimal";
+
+            input.setAttribute("id","decimal");
+            input.setAttribute("name","decimal");
+
+            output_label.innerHTML = "Binary";
+            
+            input.value = calc.last_output;
+            output.value = calc.last_input;
+
+            calc.last_input = input.value;
+            calc.last_output = output.value;
+            
+
+        break;
+
+        case "decimal_to_binary":
+
+            calc.mode = "binary_to_decimal";
+
+            input_label.setAttribute("for","binary");
+            input_label.innerHTML = "Binary";
+
+            input.setAttribute("id","binary");
+            input.setAttribute("name","binary");
+
+            output_label.innerHTML = "Decimal";
+            
+            input.value = calc.last_output;
+            output.value = calc.last_input;
+
+            calc.last_input = input.value;
+            calc.last_output = output.value;
+
+        break;
+
+    }
+
+
 }
 
 // Adding an event listener for copy to clipboard feature
@@ -118,7 +215,7 @@ async function copyToClipboard() {
     let current_notifiction = calc.notifications;
 
     // Firing the notification
-    message_box.innerHTML += `
+    notifications.innerHTML += `
     <div class="success-message-${current_notifiction}" style="width: 37rem; height: 7.2rem; margin-bottom: 3rem; animation: fade-out 5s ease-in-out forwards;">
         <div class="box" style="display: flex; justify-content: space-between; align-items: center; width: 37rem; height: 5.2rem; border-radius: 0.5rem; background: linear-gradient(90deg, #05A65B 0%, #88DE52 99.55%);">
             <span style="font-size: 2.4rem; font-weight: 400; margin: 0rem 2rem;">Copied to clipboard :)</span>
