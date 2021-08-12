545a109b93d96e54ab20c4a7ae5e20367c2c603c
Elias Biondo <contato@eliasbiondo.com>
2021-08-12 16:57:36 -0300

    :sparkles: Added 'close notification' feature

--- public/js/main.js
+++ public/js/main.js
@@ -219,7 +219,7 @@ async function copyToClipboard() {
     <div class="success-message-${current_notifiction}" style="width: 37rem; height: 7.2rem; margin-bottom: 3rem; animation: fade-out 5s ease-in-out forwards;">
         <div class="box" style="display: flex; justify-content: space-between; align-items: center; width: 37rem; height: 5.2rem; border-radius: 0.5rem; background: linear-gradient(90deg, #05A65B 0%, #88DE52 99.55%);">
             <span style="font-size: 2.4rem; font-weight: 400; margin: 0rem 2rem;">Copied to clipboard :)</span>
-            <img style="margin: 0rem 2rem; cursor: pointer;" src="public/images/icons/close_message.svg">
+            <img style="margin: 0rem 2rem; cursor: pointer;" src="public/images/icons/close_message.svg" onclick="closeCopyToClipboardNotification(${current_notifiction})">
         </div>
         <div class="progress-bar" style="width: 37rem; height: 1rem; margin: 1rem 0rem; background-color: #443D63; border-radius: 0.5rem;">
             <div class="current-progress" style="width: 65%; height: 1rem; border-radius: 0.5rem; background: linear-gradient(0deg, #05A65B 0%, #88DE52 121.75%); animation: condense 5s ease-in-out forwards;"></div>
@@ -233,3 +233,8 @@ async function copyToClipboard() {
     document.querySelector(`.success-message-${current_notifiction}`).remove();
 
 };
+
+// Implementing close "copy to clipboard" notification feature
+function closeCopyToClipboardNotification(notification_id) {
+    document.querySelector(`.success-message-${notification_id}`).remove();
+}
